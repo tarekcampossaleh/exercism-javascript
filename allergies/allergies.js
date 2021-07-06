a68dafe2fa8d84d724e00f3e6785d450a2d7116c
tarekcampossaleh <tarekcampossaleh@gmail.com>
2021-07-06 14:38:30 -0300

    implemented few structures to allergies.js

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -1,13 +1,34 @@
 export class Allergies {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
-  }
+    constructor(score) {
+        Allergies.score = score % 256;
+    }
 
-  list() {
-    
-  }
+    list() {}
 
-  allergicTo() {
-    throw new Error('Remove this statement and implement this function');
-  }
+    allergicTo(allergicItem) {
+        if (!allergicItem) return false;
+        if (Allergies.score - 2 ** items.indexOf(allergicItem) == 0)
+            return true;
+        return Allergies.score - 2 ** items.indexOf(allergicItem) > 0;
+    }
 }
+
+const items = [
+    "eggs",
+    "peanuts",
+    "shellfish",
+    "strawberries",
+    "tomatoes",
+    "chocolate",
+    "pollen",
+    "cats",
+];
+
+/* 
+
+input -> 3
+
+3 - 2**items.indexOf() // nah its broken, need to rethink 
+
+
+*/
