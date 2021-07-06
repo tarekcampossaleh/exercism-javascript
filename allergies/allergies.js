bc6d1639e9c95cf560c2f0257cfd2cffb646cef6
tarekcampossaleh <tarekcampossaleh@gmail.com>
2021-07-06 12:28:26 -0300

    added allergies exercise

---
+++ allergies/allergies.js
@@ -0,0 +1,13 @@
+export class Allergies {
+  constructor() {
+    throw new Error('Remove this statement and implement this function');
+  }
+
+  list() {
+    
+  }
+
+  allergicTo() {
+    throw new Error('Remove this statement and implement this function');
+  }
+}
--- space-age/space-age.js
+++ space-age/space-age.js
@@ -12,4 +12,5 @@ const planets = {
     neptune: 164.79132,
 };
 
+
 const earthYear = 31557600;
