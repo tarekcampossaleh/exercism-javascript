ca519f5de8c7debbf07d0d23e42b1e18391f20f4
tarekcampossaleh <tarekcampossaleh@gmail.com>
2021-07-07 17:38:54 -0300

    implemented list() initial function

--- allergies/allergies.js
+++ allergies/allergies.js
@@ -3,13 +3,31 @@ export class Allergies {
         Allergies.score = score % 256;
     }
 
-    list() {}
+    list() {
+        let returnedItems = [];
+        if (Allergies.score == 0) return [];
+        items.map((item) => {
+            if (this.allergicTo(item)) returnedItems.push(item);
+        });
+        return returnedItems;
+    }
 
     allergicTo(allergicItem) {
         if (!allergicItem) return false;
         if (Allergies.score == 2 ** items.indexOf(allergicItem)) return true;
-        if((2 ** items.indexOf(allergicItem) )*2 == 256) return Allergies.score - (2 ** items.indexOf(allergicItem)) - ((2 ** items.indexOf(allergicItem))/2) >= 0
-        return Allergies.score - (2 ** items.indexOf(allergicItem)) - ((2 ** items.indexOf(allergicItem))*2) >= 0;
+        if (2 ** items.indexOf(allergicItem) * 2 == 256)
+            return (
+                Allergies.score -
+                    2 ** items.indexOf(allergicItem) -
+                    2 ** items.indexOf(allergicItem) / 2 >=
+                0
+            );
+        return (
+            Allergies.score -
+                2 ** items.indexOf(allergicItem) -
+                2 ** items.indexOf(allergicItem) * 2 >=
+            0
+        );
     }
 }
 
@@ -30,5 +48,8 @@ input -> 3
 
 3 - 2**items.indexOf() // nah its broken, need to rethink 
 
+const allergies = new Allergies(1);
+
+console.log(allergies.allergicTo('cats'))
 
 */
