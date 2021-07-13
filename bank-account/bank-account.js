c849a0c8421ebf2cb59f9ad6f0b88ae24ac1d46f
tarekcampossaleh <tarekcampossaleh@gmail.com>
2021-07-13 12:04:21 -0300

    fix: bank-account solution

--- bank-account/bank-account.js
+++ bank-account/bank-account.js
@@ -1,8 +1,3 @@
-//
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class BankAccount {
     constructor() {
         this._balance = 0;
@@ -43,5 +38,3 @@ export class ValueError extends Error {
         super("Bank account error");
     }
 }
-
-// throw new ValueError()
